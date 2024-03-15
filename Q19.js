a9b5521e8e7ad569027b0359eb0a088c9bd5c5d7
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-03-15 18:13:45 +0500

    Add files via upload

---
+++ Q19.js
@@ -0,0 +1,5 @@
+"use strict";
+let guestList = ["salma", "Sana", "Sara", "Sadaf"];
+//guestList.forEach(oneGuest =>console.log(`Salam,${oneGuest},I would like to invite you at dinner`));
+let lengthguestList = guestList.length;
+console.log(lengthguestList);
---
+++ Q19.ts
@@ -0,0 +1,5 @@
+let guestList = ["salma","Sana", "Sara","Sadaf"];
+
+//guestList.forEach(oneGuest =>console.log(`Salam,${oneGuest},I would like to invite you at dinner`));
+let lengthguestList:number = guestList.length;
+console.log(lengthguestList);
